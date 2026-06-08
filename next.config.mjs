e6a02fa13b655f4ba7f84705b624b149b875046a
halahit <hithytl15@gmail.com>
2026-06-08 03:29:56 +0300

    add standalone output

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,7 @@ const withPWA = withPWAInit({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**" },
