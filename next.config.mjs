95a3d9d4f4b0b8820036e6a43d1aa98d25cf8a84
halahit <hithytl15@gmail.com>
2026-06-09 22:45:04 +0300

    fix image caching

--- next.config.mjs
+++ next.config.mjs
@@ -17,7 +17,7 @@ const nextConfig = {
       { protocol: "https", hostname: "**" },
       { protocol: "http", hostname: "localhost" },
     ],
-    unoptimized: true,
+    minimumCacheTTL: 60 * 60 * 24 * 30,
   },
   experimental: {
     serverActions: {
